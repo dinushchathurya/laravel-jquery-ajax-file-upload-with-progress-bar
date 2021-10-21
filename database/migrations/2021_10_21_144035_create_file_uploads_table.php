28c04576e05940aa44892e58481f95a6593d5326
dinushchathurya <dinushchathurya21@gmail.com>
2021-10-21 20:15:31 +0530

    feat: Create migration file

---
+++ database/migrations/2021_10_21_144035_create_file_uploads_table.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFileUploadsTable extends Migration
+{
+
+    public function up()
+    {
+        Schema::create('file_uploads', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('file_uploads');
+    }
+}
